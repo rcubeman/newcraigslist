b99237244b9954ce3139fa66845c08deb88f38e5
Brandon Haynes <83brandonh@gmail.com>
2018-01-30 18:17:19 -0500

    added toggleclass to each p

--- app.js
+++ app.js
@@ -1,21 +1,10 @@
-// var adElements = document.querySelectorAll('.ad');
-// adElements.forEach(function(adElement) {
-// adElement.addEventListener('click', function(event) {
-  //      var ad = event.target.closest('.ad');
- //    var adParagraph = ad.querySelector('p');
-//    adParagraph.style.backgroundColor = 'yellow';});  
-    // });
-        
-    
-    div.addEventListner('click', toggleClass )
+
+    div.addEventListener('click', toggleClass )
          function toggleClass (el){
         if(el.className == "highlighton"){
-            el.className = "highlightoff";
+            el.className = "highlightoff";        
         } else {
-            el.className = "highlighton";
+            el.className = "highlighton";  
         }
     }
-function save() {
-    var clicksave = document.getElementsByClassName('ad').value;
-    localStorage.setItem('click',clicksave);
-}
+
