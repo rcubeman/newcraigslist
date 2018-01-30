420a833ddd77734b9a5e93ab41dffdb944543e71
Brandon Haynes <83brandonh@gmail.com>
2018-01-29 20:36:22 -0500

    moved JS to JS file

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-var adElements = document.querySelectorAll('.ad');
-adElements.forEach(function(adElement) {
-    adElement.addEventListener('click', function(event) {
-        var ad = event.target.closest('.ad');
-        var adParagraph = ad.querySelector('p');
-        adParagraph.style.backgroundColor = 'yellow';});  
-    });
+// var adElements = document.querySelectorAll('.ad');
+// adElements.forEach(function(adElement) {
+// adElement.addEventListener('click', function(event) {
+  //      var ad = event.target.closest('.ad');
+ //    var adParagraph = ad.querySelector('p');
+//    adParagraph.style.backgroundColor = 'yellow';});  
+    // });
         
+    
+    div.addEventListner('click', toggleClass )
+         function toggleClass (el){
+        if(el.className == "highlighton"){
+            el.className = "highlightoff";
+        } else {
+            el.className = "highlighton";
+        }
+    }
+function save() {
+    var clicksave = document.getElementsByClassName('ad').value;
+    localStorage.setItem('click',clicksave);
+}
