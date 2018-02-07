a7b719c91ef71c41d9ca735521c285f9b349226a
Brandon Haynes <83brandonh@gmail.com>
2018-02-06 20:07:20 -0500

    added in functions to locally store which ad was clicked

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+var adElements = document.querySelectorAll('.ad');
 
-    div.addEventListener('click', toggleClass )
-         function toggleClass (el){
-        if(el.className == "highlighton"){
-            el.className = "highlightoff";        
-        } else {
-            el.className = "highlighton";  
-        }
+adElements.forEach(function(adElement) {
+    adElement.addEventListener('click', highlightOnClick);  
+    adElement.addEventListener('click', saveFavorite);
+});
+
+function highlightOnClick(event) {
+    var ad = event.target.closest('.ad');
+    var adParagraph = ad.querySelector('p');
+    adParagraph.classList.toggle('highlighton');
+}
+
+function saveFavorite(event) {
+    var ad = event.target.closest('.ad');
+    var numcaught = ad.dataset.idnum;
+    var favoritesCsv = localStorage.getItem('favorites');
+    var favorites = favoritesCsv.split(",");
+
+    if (favorites.indexOf(numcaught) > -1) {
+        var numIndex = favorites.indexOf(numcaught);
+        favorites.splice(numIndex,1);
+    } else {
+        favortites.push(numcaught);
     }
-
+}
